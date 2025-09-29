1c0b748be0010dd1827eb497bd0bdd6f7cdc5430
KubaTaba1uga <luna7337anul@proton.me>
2025-09-29 21:32:37 +0200

    add optee compilation script

--- scripts/build.mk
+++ scripts/build.mk
@@ -11,4 +11,15 @@ U_BOOT_BIN		:= u-boot.bin
 U_BOOT_DTB		:= u-boot.dtb
 
 AARCH32_GCC_VERSION 		?= arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf
-SRC_AARCH32_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/$(AARCH32_GCC_VERSION).tar.xz
+SRC_AARCH32_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf.tar.xz
+AARCH32_CROSS_COMPILE 		?= $(AARCH32_PATH)/bin/arm-linux-gnueabihf-
+
+OPTEE_OS_COMMON_FLAGS ?= \
+	$(OPTEE_OS_COMMON_EXTRA_FLAGS) \
+	PLATFORM=stm32mp1-135F_DK \
+	CROSS_COMPILE=$(CROSS_COMPILE_S_USER) \
+	$(OPTEE_OS_TA_CROSS_COMPILE_FLAGS) \
+	CFG_TEE_CORE_LOG_LEVEL=3 \
+	DEBUG=1 
+
+
---
+++ scripts/compile_optee.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+OPTEE_OS_COMMON_FLAGS="PLATFORM=stm32mp1-135F_DK CFG_USER_TA_TARGETS=ta_arm32 ARCH=arm CFG_ARM32_core=y CROSS_COMPILE=$SCRIPT_DIR/../third_party/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf- CFG_SCP_FIRMWARE=$SCRIPT_DIR/../third_party/scp-firmware CFG_TEE_CORE_LOG_LEVEL=3 CFG_UNWIND=y DEBUG=1 CFG_EMBED_DTB_SOURCE_FILE=stm32mp135f-dk.dts"
+
+cd third_party/optee-os/
+
+make $OPTEE_OS_COMMON_FLAGS
+
+mkdir ../../build
+cp out/third_party/optee-os/out/arm-plat-stm32mp1/core/tee.bin ../../build/
+cp tee-raw.bin ../../build/
+cp tee-*_v2.bin ../../build/
+
