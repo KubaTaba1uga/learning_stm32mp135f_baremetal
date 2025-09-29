ff671fc92ccd38d35d962c542df4040e26285e4e
KubaTaba1uga <luna7337anul@proton.me>
2025-09-29 20:43:00 +0200

    WIP

---
+++ scripts/build.mk
@@ -0,0 +1,14 @@
+BREXT_FLAVOR		= STM32MP135F-DK
+STM32MP1_DTS_BASENAME	= stm32mp135f-dk
+STM32MP1_DEFCONFIG_U_BOOT = stm32mp13_defconfig
+
+TFA_BIN			:= tf-a-$(STM32MP1_DTS_BASENAME).stm32
+TFA_FIP_BIN		:= fip.bin
+OPTEE_HEADER_BIN	:= tee-header_v2.bin
+OPTEE_PAGER_BIN		:= tee-pager_v2.bin
+OPTEE_PAGEABLE_BIN  	:= tee-pageable_v2.bin
+U_BOOT_BIN		:= u-boot.bin
+U_BOOT_DTB		:= u-boot.dtb
+
+AARCH32_GCC_VERSION 		?= arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf
+SRC_AARCH32_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/$(AARCH32_GCC_VERSION).tar.xz
