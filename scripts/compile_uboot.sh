9dce601d78c01bf2d021e97a79fa39070f15753c
KubaTaba1uga <luna7337anul@proton.me>
2025-09-30 23:22:13 +0200

    save working build

--- scripts/compile_uboot.sh
+++ scripts/compile_uboot.sh
@@ -8,8 +8,8 @@ cd third_party/u-boot
 # export CROSS_COMPILE="$SCRIPT_DIR/../third_party/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-"
 # make stm32mp13_defconfig 
 # make DEVICE_TREE=stm32mp135f-dk all
-CROSS_COMPILE="/usr/bin/ccache /home/taba1uga/Github/optee_bundle/build/../toolchains/aarch32/bin/arm-linux-gnueabihf-" make stm32mp13_defconfig 
-CROSS_COMPILE="/usr/bin/ccache /home/taba1uga/Github/optee_bundle/build/../toolchains/aarch32/bin/arm-linux-gnueabihf-" make DEVICE_TREE=stm32mp135f-dk all
+CROSS_COMPILE="/usr/bin/ccache /home/taba1uga/Github/learning_stm32mp135f_baremetal/third_party/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-" make stm32mp13_defconfig 
+CROSS_COMPILE="/usr/bin/ccache /home/taba1uga/Github/learning_stm32mp135f_baremetal/third_party/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-" make DEVICE_TREE=stm32mp135f-dk all -j 2
 
 
 mkdir -p ../../build
