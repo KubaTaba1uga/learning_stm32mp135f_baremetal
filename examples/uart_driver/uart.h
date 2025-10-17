840136923a874cac3ca7cd62491e25c74de4dd1c
KubaTaba1uga <luna7337anul@proton.me>
2025-10-17 19:47:14 +0200

    save progress

--- examples/uart_driver/uart.h
+++ examples/uart_driver/uart.h
@@ -2,6 +2,7 @@
 #define UART_H
 
 #include "common.h"
+#include <cstdint>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -35,33 +36,6 @@ struct uart {
 #define UART7 ((struct uart *)0x40018000U)
 #define UART8 ((struct uart *)0x40019000U)
 
-enum {
-  UART_CR1_UE,
-  UART_CR1_UESM,
-  UART_CR1_RE,
-  UART_CR1_TE,
-  UART_CR1_IDLEIE,
-  UART_CR1_RXFNEIE,
-  UART_CR1_TCIE,
-  UART_CR1_TXFNFIE,
-  UART_CR1_PEIE,
-  UART_CR1_PS,
-  UART_CR1_PCE,
-  UART_CR1_WAKE,
-  UART_CR1_M0,
-  UART_CR1_MME,
-  UART_CR1_CMIE,
-  UART_CR1_OVER8,
-  UART_CR1_DEDT,
-  UART_CR1_DEAT,
-  UART_CR1_RTOIE,
-  UART_CR1_EOBIE,
-  UART_CR1_M1,
-  UART_CR1_FIFOEN,
-  UART_CR1_TXFEIE,
-  UART_CR1_RXFFIE,
-};
-
 static inline void uart_init(struct uart *uart) {
   { // Reset UART
     uart->CR1 = 0;
@@ -87,6 +61,131 @@ static inline void uart_init(struct uart *uart) {
     uart->VERR = 0x00000024;
     uart->IPIDR = 0x00130003;
     uart->SIDR = 0xA3C5DD01;
+  }
+}
+
+enum {
+  UART_CR1_ALT_UE,
+  UART_CR1_ALT_UESM,
+  UART_CR1_ALT_RE,
+  UART_CR1_ALT_TE,
+  UART_CR1_ALT_IDLEIE,
+  UART_CR1_ALT_RXFNEIE,
+  UART_CR1_ALT_TCIE,
+  UART_CR1_ALT_TXEIE,
+  UART_CR1_ALT_PEIE,
+  UART_CR1_ALT_PS,
+  UART_CR1_ALT_PCE,
+  UART_CR1_ALT_WAKE,
+  UART_CR1_ALT_M0,
+  UART_CR1_ALT_MME,
+  UART_CR1_ALT_CMIE,
+  UART_CR1_ALT_OVER8,
+  UART_CR1_ALT_DEDT,
+  UART_CR1_ALT_DEAT = UART_CR1_ALT_DEDT + 6,
+  UART_CR1_ALT_RTOIE,
+  UART_CR1_ALT_EOBIE,
+  UART_CR1_ALT_M1,
+  UART_CR1_ALT_FIFOEN,
+  UART_CR2_SLVEN = UART_CR1_ALT_FIFOEN + 3,
+  UART_CR2_NSS = UART_CR2_SLVEN + 3,
+  UART_CR2_ADDM7,
+  UART_CR2_LBDL,
+  UART_CR2_LBDIE,
+  UART_CR2_LBCL = UART_CR2_LBDIE + 2,
+  UART_CR2_CPHA,
+  UART_CR2_CPOL,
+  UART_CR2_CLKEN,
+  UART_CR2_STOP,
+  UART_CR2_LINEN = UART_CR2_STOP + 2,
+  UART_CR2_SWAP,
+  UART_CR2_RXINV,
+  UART_CR2_TXINV,
+  UART_CR2_DATAINV,
+  UART_CR2_MSBFIRST,
+  UART_CR2_ABREN,
+  UART_CR2_ABRMOD,
+  UART_CR2_RTOEN,
+  UART_CR2_ADD,
+  UART_CR3_EIE = UART_CR2_ADD + 9,
+  UART_CR3_IREN,
+  UART_CR3_IRLP,
+  UART_CR3_HDSEL,
+  UART_CR3_NACK,
+  UART_CR3_SCEN,
+  UART_CR3_DMAR,
+  UART_CR3_DMAT,
+  UART_CR3_RTSE,
+  UART_CR3_CTSE,
+  UART_CR3_CTSIE,
+  UART_CR3_ONEBIT,
+  UART_CR3_OVRDIS,
+  UART_CR3_DDRE,
+  UART_CR3_DEM,
+  UART_CR3_DEP,
+  UART_CR3_SCARCNT = UART_CR3_DEP + 2,
+  UART_CR3_WUS = UART_CR3_SCARCNT + 3,
+  UART_CR3_WUFIE,
+  UART_CR3_TXFTIE,
+  UART_CR3_TCBGTIE,
+  UART_CR3_RXTCFG,
+  UART_CR3_RFXTIE = UART_CR3_RXTCFG + 3,
+  UART_CR3_TXTCFG,
+};
+
+static inline void uart_set_ctrl(struct uart *uart, uint8_t setting,
+                                  uint8_t value) {
+  if (setting < UART_CR2_SLVEN) { // Handle CR1
+    switch (setting) {
+    case UART_CR1_ALT_DEAT:
+      BITS_SET(uart->CR1, value, 31, setting);
+      break;
+    default:
+      if (value) {
+        BIT_SET(uart->CR1, setting);
+      } else {
+        BIT_CLEAR(uart->CR1, setting);
+      }
+    }
+  } else if (setting < UART_CR3_EIE) { // Handle CR2
+    uint8_t bits = setting - UART_CR2_SLVEN;
+    switch (setting) {
+    case UART_CR2_STOP:
+    case UART_CR2_ABRMOD:
+      BITS_SET(uart->CR2, value, 3U, bits);
+      break;
+    case UART_CR2_ADD:
+      BITS_SET(uart->CR1, value, 0xFF, bits);
+      break;
+    default:
+      if (value) {
+        BIT_SET(uart->CR1, bits);
+      } else {
+        BIT_CLEAR(uart->CR1, bits);
+      }
+    }
+  } else { // Handle CR3
+    uint8_t bits = setting -  UART_CR3_EIE;
+    switch (setting) {
+    case UART_CR3_SCARCNT:
+    case UART_CR3_RXTCFG: 
+    case UART_CR3_TXTCFG:           
+      BITS_SET(uart->CR3, value, 7U, bits);
+      break;      
+    case UART_CR3_WUS:
+      BITS_SET(uart->CR3, value, 3U, bits);
+      break;
+    case UART_CR2_ADD:
+      BITS_SET(uart->CR1, value, 0xFF, bits);
+      break;
+    default:
+      if (value) {
+        BIT_SET(uart->CR1, bits);
+      } else {
+        BIT_CLEAR(uart->CR1, bits);
+      }
+    }
+    
   }
 }
 
