4a2a5593fc5214014773d7dbd9e167c30b6ebbe1
KubaTaba1uga <luna7337anul@proton.me>
2025-10-24 12:18:13 +0200

    save progress

--- examples/uart_interrupt_rx/main.c
+++ examples/uart_interrupt_rx/main.c
@@ -50,11 +50,53 @@ value wich we got from IAR into GICC_EOIR.
 const char word[] = "Hello friend";
 void print_banner(struct uart *uart);
 void default_interrupt_handler(void);
+#include <stdint.h>
+
+#define GICD_BASE   0xA0021000u  // GIC Distributor
+#define GICC_BASE   0xA0022000u  // GIC CPU Interface
+
+#define GICD_CTLR       0x000u
+#define GICD_ISENABLER0 0x100u
+#define GICD_SGIR 0xF00u
+
+#define GICC_CTLR  0x000u
+#define GICC_PMR   0x004u
+
+static inline void wr32(uintptr_t addr, uint32_t val) { *(volatile uint32_t *)addr = val; }
+static inline void dsb(void){ __asm__ volatile("dsb sy" ::: "memory"); }
+static inline void isb(void){ __asm__ volatile("isb" ::: "memory"); }
+
+void gic_enable_sgis(void)
+{
+    /* 1) Disable CPU interface and Distributor */
+    wr32(GICC_BASE + GICC_CTLR, 0x00000000u);
+    dsb(); isb();
+    wr32(GICD_BASE + GICD_CTLR, 0x00000000u);
+    dsb(); isb();
+
+    GICD->CPENDSGIR[0] = 0xFFFFFFFF;
+    GICD->CPENDSGIR[1] = 0xFFFFFFFF;
+    GICD->CPENDSGIR[2] = 0xFFFFFFFF;
+    GICD->CPENDSGIR[3] = 0xFFFFFFFF;
+
+    /* 2) Enable SGIs 0..15 (set bits 0..15). 
+          If you really want 0..16 inclusive, use 0x0001FFFFu instead. */
+    wr32(GICD_BASE + GICD_ISENABLER0, 0x0000FFFFu);   // SGI0..15 enabled
+
+    /* (Recommended) Allow all priorities through the CPU interface */
+    wr32(GICC_BASE + GICC_PMR, 0xFFu);
+
+    /* 3) Re-enable Distributor (Grp0 + Grp1) and CPU interface (Grp0 + Grp1) */
+    wr32(GICD_BASE + GICD_CTLR, (1u << 0) | (1u << 1));   // ENABLEGRP0 | ENABLEGRP1
+    dsb(); isb();
+    wr32(GICC_BASE + GICC_CTLR, (1u << 0) | (1u << 1));   // EnableGrp0 | EnableGrp1
+    dsb(); isb();
+}
 
 int main(void) {
   uart_write_str(UART4, __func__);
   uart_write_str(UART4, "\r\n");
-  
+
   print_banner(UART4);
 
   { // Enable clock for GPIOB and GPIOC
@@ -95,7 +137,7 @@ int main(void) {
 
     // Set baud rate (assuming 64MHz HSI clock, 115200 baud)
     uart_set_baud_rate(USART1, 64000000, 115200);
-    uart_set_ctrl(USART1, UART_CR1_ALT_RXFNEIE, 1U); // Enable IRQ
+    /* uart_set_ctrl(USART1, UART_CR1_ALT_RXFNEIE, 1U); // Enable IRQ */
     uart_set_ctrl(USART1, UART_CR1_ALT_RE, 1U);      // Enable reciver
     uart_set_ctrl(USART1, UART_CR1_ALT_TE, 1U);      // Enable transiver
     uart_set_ctrl(USART1, UART_CR1_ALT_UE, 1U);      // Enable uart
@@ -106,15 +148,87 @@ int main(void) {
   }
 
   { // Configure General interrupt controller
+    /* gicd_init(GICD); */
     gicd_set_cpu0_for_usart1(GICD);
     gicd_set_priority_for_usart1(GICD);
     gicd_enable_usart1(GICD);
 
+    /* gicc_init(GICC); */
+
+
     gicc_set_pmr(GICC);
     gicc_enable_cpu(GICC);
   }
+  
+  if (GICD->ISENABLER[0] & 0b1) {
+    uart_write_str(UART4, "SIG0=enabled");
+  } else {
+    uart_write_str(UART4, "SIG0=disabled");
+  }
+  uart_write_str(UART4, "\r\n");
 
-  uart_write_str(USART1, "Done");
+  if (GICD->ISENABLER[0] & 0b10) {
+    uart_write_str(UART4, "SIG1=enabled");
+  } else {
+    uart_write_str(UART4, "SIG1=disabled");
+  }
+  uart_write_str(UART4, "\r\n");
+
+  
+  if (GICD->ISENABLER[2] & (1U<<6)) {
+    uart_write_str(UART4, "USART1=enabled");
+  } else {
+    uart_write_str(UART4, "USART1=disabled");
+  }
+  uart_write_str(UART4, "\r\n");
+
+  GICD->ISENABLER[2] = (1U<<6)  ;
+  if (GICD->ISENABLER[2] & (1U<<6)) {
+    uart_write_str(UART4, "USART1=enabled");
+  } else {
+    uart_write_str(UART4, "USART1=disabled");
+  }
+  uart_write_str(UART4, "\r\n");
+
+  if (GICD->SPENDSGIR[0] & 0b1) {
+    uart_write_str(UART4, "SIG0=pending");
+  } else {
+    uart_write_str(UART4, "SIG0=not pending");
+  }
+  uart_write_str(UART4, "\r\n");
+  
+  /* GICD->ISACTIVER[0] = 0b1; */
+  if (GICD->ISACTIVER[0] & 0b1) {
+    uart_write_str(UART4, "SIG0=active");
+  } else {
+    uart_write_str(UART4, "SIG0=not active");
+  }
+  uart_write_str(UART4, "\r\n");
+
+  if (GICD->IGROUPR[0] & 0b1) {
+    uart_write_str(UART4, "SIG0=group1");
+  } else {
+    uart_write_str(UART4, "SIG0=group0");
+  }
+  uart_write_str(UART4, "\r\n");
+  
+  uint32_t secure = (*(uint32_t *)(0x5C007000 + 0x14)) & 0b11;
+  switch (secure) {
+  case 0b00:
+    uart_write_str(UART4, "USART1=read secure and write secure");
+    break;
+  case 0b01:
+    uart_write_str(UART4, "USART1=read non-secure and write secure");
+    break;
+  case 0b10:
+    uart_write_str(UART4, "USART1=reserved");
+    break;
+  case 0b11:
+    uart_write_str(UART4, "USART1=read non-secure and write non-secure");
+    break;
+  }
+
+
   while (1) {
   }
 
@@ -124,7 +238,7 @@ int main(void) {
 void print_banner(struct uart *uart) {
   uart_write_str(UART4, __func__);
   uart_write_str(UART4, "\r\n");
-  
+
   for (uint8_t i = 0; i < 32; i++) {
     uart_write_str(uart, "\r\n");
   }
@@ -136,20 +250,39 @@ void print_banner(struct uart *uart) {
 }
 
 void default_interrupt_handler(void) {
-  uart_write_str(UART4, __func__);
-  uart_write_str(UART4, "\r\n");
+  uint32_t iar = GICC->IAR;
+  uint32_t id  = iar & 0x3FFu;          // INTID in [9:0]
+
+  uart_write_str(UART4, "IRQ start\r\n");
   
-  uart_write_char(UART4, 'I');
+  // 1023 == spurious on GICv2; just return
+  if (id == 1023u){
+    uart_write_str(UART4, "IRQ=invalid\r\n");
+    return;    
+    } 
 
-  uint32_t intrrpt_id = GICC->AIAR;
+  if (id < 16u) {                        // SGI (0..15)
+    // handle SGI 'id' here if you want to log/branch
+    GICC->EOIR = iar;                    // write back EXACT IAR value
+    GICC->DIR = id;                      // optional but fine on v2 1N model
+    if (id==0){
+    uart_write_str(UART4, "IRQ=SGI0\r\n");    
+    } else {
+    uart_write_str(UART4, "IRQ=SGI1\r\n");          
+    }
 
-  uart_read_char(USART1);
-  /* uint32_t i= 0xFFFF; */
-  /* while (i--) { */
-  /* } */
+    return;
+  }
 
-  uart_write_char(UART4, 'X');
+  if (id == 70u) {                       // USART1 (SPI)
+  uart_write_str(UART4, "IRQ=USART1\r\n");
+    (void)uart_read_char(USART1);        // clear UART condition
+    GICC->EOIR = iar;                    // EOI with full IAR
+    return;
+  }
 
-  GICC->AEOIR = intrrpt_id;
-  /* GICC->DIR = intrrpt_id;   */
-};
+  uart_write_str(UART4, "IRQ=other\r\n");
+  
+  // Other interrupts (if any)
+  GICC->EOIR = iar;
+}
--- shared/gic.h
+++ shared/gic.h
@@ -37,8 +37,8 @@ struct gicd {
   volatile uint8_t _reserved11[488];
   volatile uint32_t SGIR;
   volatile uint8_t _reserved12[12];
-  volatile uint32_t CPENDSGIR[4];
-  volatile uint32_t SPENDSGIR[4];
+  volatile uint32_t CPENDSGIR[4]; // Clear pengin interrupt
+  volatile uint32_t SPENDSGIR[4]; // Set/read pending interrupt
   volatile uint8_t _reserved13[160];
   volatile uint32_t PIDR4;
   volatile uint32_t PIDR5;
@@ -80,23 +80,133 @@ struct gicc {
 #define GICD ((struct gicd *)0xA0021000)
 #define GICC ((struct gicc *)0xA0022000)
 
+static inline void gicd_init(struct gicd *gicd) {
+  BITS_SET(gicd->CTLRNS, 0, 1, 0);
+
+  BITS_SET(gicd->TYPER, 0b00000101, 0xFF, 0);
+  BITS_SET(gicd->TYPER, 0b111111, 0b111111, 11);
+
+  BITS_SET(gicd->IIDR, 0b00000001, 0xFF, 24);
+  BITS_SET(gicd->IIDR, 0b00000001010000111011, 0b11111111111111111111, 0);
+
+  for (uint8_t i = 0; i < sizeof(gicd->IGROUPR) / sizeof(uint32_t); i++) {
+    gicd->IGROUPR[i] = 0;
+  }
+
+  BITS_SET(gicd->ISENABLER[0], 0xFFFF, 0xFFFF, 16);
+  BITS_SET(gicd->ISENABLER[0], 0, 0b1111111, 25);
+  for (uint8_t i = 1; i < sizeof(gicd->IGROUPR) / sizeof(uint32_t); i++) {
+    gicd->ISENABLER[i] = 0;
+  }
+
+  BITS_SET(gicd->ICENABLER[0], 0xFFFF, 0xFFFF, 16);
+  BITS_SET(gicd->ICENABLER[0], 0, 0b1111111, 25);
+  for (uint8_t i = 1; i < sizeof(gicd->IGROUPR) / sizeof(uint32_t); i++) {
+    gicd->ICENABLER[i] = 0;
+  }
+
+  for (uint8_t i = 0; i < sizeof(gicd->ISPENDR) / sizeof(uint32_t); i++) {
+    gicd->ISPENDR[i] = 0;
+  }
+
+  for (uint8_t i = 0; i < sizeof(gicd->ICPENDR) / sizeof(uint32_t); i++) {
+    gicd->ICPENDR[i] = 0;
+  }
+
+  for (uint8_t i = 0; i < sizeof(gicd->ISACTIVER) / sizeof(uint32_t); i++) {
+    gicd->ISACTIVER[i] = 0;
+  }
+
+  for (uint8_t i = 0; i < sizeof(gicd->ICACTIVER) / sizeof(uint32_t); i++) {
+    gicd->ICACTIVER[i] = 0;
+  }
+
+  for (uint8_t i = 0; i < sizeof(gicd->IPRIORITYR) / sizeof(uint32_t); i++) {
+    BITS_SET(gicd->IPRIORITYR[i], 0, 0b11111, 3);
+    BITS_SET(gicd->IPRIORITYR[i], 0, 0b11111, 11);
+    BITS_SET(gicd->IPRIORITYR[i], 0, 0b11111, 19);
+    BITS_SET(gicd->IPRIORITYR[i], 0, 0b11111, 27);
+  }
+
+  for (uint8_t i = 8; i < sizeof(gicd->ITARGETSR) / sizeof(uint32_t); i++) {
+    BITS_SET(gicd->ITARGETSR[i], 0, 0b11, 0);
+    BITS_SET(gicd->ITARGETSR[i], 0, 0b11, 8);
+    BITS_SET(gicd->ITARGETSR[i], 0, 0b11, 16);
+    BITS_SET(gicd->ITARGETSR[i], 0, 0b11, 24);
+  }
+
+  gicd->ICFGR[0] = 0b10101010101010101010101010101010;
+  BITS_SET(gicd->ICFGR[1], 0b01010101010101, 0b11111111111111, 18);
+  for (uint8_t i = 2; i < sizeof(gicd->ICFGR) / sizeof(uint32_t); i++) {
+    gicd->ICFGR[i] = 0b01010101010101010101010101010101;
+  }
+
+  BITS_SET(gicd->PPISR, 0, 0b1111111, 9);
+
+  for (uint8_t i = 0; i < sizeof(gicd->SPISR) / sizeof(uint32_t); i++) {
+    gicd->SPISR[i] = 0;
+  }
+
+  for (uint8_t i = 8; i < sizeof(gicd->CPENDSGIR) / sizeof(uint32_t); i++) {
+    BITS_SET(gicd->CPENDSGIR[i], 0, 0b11, 0);
+    BITS_SET(gicd->CPENDSGIR[i], 0, 0b11, 8);
+    BITS_SET(gicd->CPENDSGIR[i], 0, 0b11, 16);
+    BITS_SET(gicd->CPENDSGIR[i], 0, 0b11, 24);
+  }
+  
+  for (uint8_t i = 8; i < sizeof(gicd->SPENDSGIR) / sizeof(uint32_t); i++) {
+    BITS_SET(gicd->SPENDSGIR[i], 0, 0b11, 0);
+    BITS_SET(gicd->SPENDSGIR[i], 0, 0b11, 8);
+    BITS_SET(gicd->SPENDSGIR[i], 0, 0b11, 16);
+    BITS_SET(gicd->SPENDSGIR[i], 0, 0b11, 24);
+  }
+
+  gicd->PIDR0 = 0b1001;  
+  gicd->PIDR1 = 0b00101101;
+  gicd->PIDR2 = 0b110101;
+  gicd->PIDR3 = 0;
+  gicd->PIDR4 = 0b001;  
+  
+  gicd->PIDR5 = 0;
+  gicd->PIDR6 = 0;
+  gicd->PIDR7 = 0;
+
+  gicd->CIDR[0] = 0b1011;
+  gicd->CIDR[1] = 0b00001111;
+  gicd->CIDR[2] = 0b101;
+  gicd->CIDR[3] = 0b10001101;
+    
+}
+
 static inline void gicd_set_cpu0_for_usart1(struct gicd *gicd) {
   // usart1 is id=70
 
   // 70 = 4 * 17 + 2;
   BITS_SET(gicd->ITARGETSR[17], // Set cpu (for interrupt number 68-71)
-           0b01,                   // Cpu Number 0, 01 for cpu 0, 10 for cpu 1
+           0b01,                // Cpu Number 0, 01 for cpu 0, 10 for cpu 1
            0b11,                // Max field value 0b11
            16                   // Start at bit 16th (select interrupt id 68+2)
   );
 }
+
+static inline void gicd_set_cpu0_for_sig0(struct gicd *gicd) {
+  // usart1 is id=70
+
+  // 70 = 4 * 17 + 2;
+  BITS_SET(gicd->ITARGETSR[0], // Set cpu (for interrupt number 68-71)
+           0b01,                // Cpu Number 0, 01 for cpu 0, 10 for cpu 1
+           0b11,                // Max field value 0b11
+           0                   // Start at bit 16th (select interrupt id 68+2)
+  );
+}
+
 
 static inline void gicd_set_priority_for_usart1(struct gicd *gicd) {
   // usart1 is id=70
 
   // 70 = 4 * 17 + 2;
   BITS_SET(gicd->IPRIORITYR[17], // Set interrupt priority
-           0,           // Set maximum priority
+           0,                    // Set maximum priority
            0b11111000,           // Max field value 0b11111000
            16                    // Start at bit 16th
   );
@@ -104,31 +214,56 @@ static inline void gicd_set_priority_for_usart1(struct gicd *gicd) {
 
 static inline void gicd_enable_usart1(struct gicd *gicd) {
   // usart1 is id=70
-
-  // 70 = 16 * 4 + 6;
-  /* BITS_SET(gicd->ICFGR[4], 0b00, 0b11,   */
-  /* 	   12); // Set edged triggered interrupt */
-
-  // 70 = 32 * 2 + 6;
-  BIT_SET(gicd->ISENABLER[2], // Use x=2
-
-          6);
-
+  /* BIT_SET(GICD->CPENDSGIR[0], 0); */
   BIT_SET(gicd->IGROUPR[2], // Use group1 for usart1
           6);
 
+  // 70 = 16 * 4 + 6;
+  BITS_SET(gicd->ICFGR[4], 0b00, 0b11,
+           12); // Set level triggered interrupt
+
+  // 70 = 32 * 2 + 6;
+  BIT_SET(gicd->ISENABLER[2], // Enable interrupt handling for usart 1
+          6);
 }
 
+static inline void gicc_init(struct gicc*gicc){
+  BIT_CLEAR(gicc->CTLRNS, 0);
+  BIT_CLEAR(gicc->CTLRNS, 5);
+  BIT_CLEAR(gicc->CTLRNS, 6);
+  BIT_CLEAR(gicc->CTLRNS, 9);
+
+  BITS_SET(gicc->PMR, 0, 0b11111, 3);
+
+  BITS_SET(gicc->BPRNS, 0b011, 0b111, 0);
+
+  BITS_SET(gicc->IAR, 0b11111111110, 0b11111111111, 0);
+
+  BITS_SET(gicc->RPR, 0b11111, 0b11111, 3);
+
+  BITS_SET(gicc->HPPIR, 0b11111111110, 0b11111111111, 0);
+
+  BITS_SET(gicc->ABPR, 0b110, 0b111, 0);
+
+  BITS_SET(gicc->AIAR, 0b11111111110, 0b11111111111, 0);
+
+  BITS_SET(gicc->AHPPIR, 0b11111111110, 0b11111111111, 0);
+
+  gicc->APR0 = 0;
+  gicc->NSAPR0 = 0;
+
+  gicc->IIDR = 0b110111000101000010000001000000;  
+  }
+
+
 static inline void gicc_set_pmr(struct gicc *gicc) {
-  BITS_SET(gicc->PMR, 0b11111, 0b11111, 3);
+  BITS_SET(gicc->PMR, 0xFF, 0xFF, 3);
 }
 
 static inline void gicc_enable_cpu(struct gicc *gicc) {
-  BIT_SET(gicc->CTLRNS, 0);
-  BIT_SET(gicc->CTLRNS, 5);  
-  BIT_SET(gicc->CTLRNS, 9);  
+  BIT_SET(gicc->CTLRNS, 0); // Enable group 0 interrupt for cpu 0
+  BIT_SET(gicc->CTLRNS, 1); // Enable group 1 interrupt for cpu 0                            // 
+  BIT_SET(gicc->CTLRNS, 9); // Do not require DIR for ack, only EOIR
 }
-
-
 
 #endif // GIC_H
